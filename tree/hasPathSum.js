0bc2b70edf8ca5535f08942df05b5aa5e7bd7ef0
enaoi <charlotte.xuuuu@gmail.com>
2020-07-03 18:30:03 +0800

    change floder

--- tree/hasPathSum.js
+++
@@ -1,9 +0,0 @@
-
-var hasPathSum = function (root, sum) {
-  if (!root) return false;
-  sum = sum - root.val;
-  if (root.left === null && root.right === null) {
-    return sum === 0
-  }
-  return hasPathSum(root.left, sum) || hasPathSum(root.right, sum)
-}
